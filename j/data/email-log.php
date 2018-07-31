536e1f73f5fc339f33c52c114e752491bc563865
ukdioninvestment <cashfarmnigeria@gmail.com>
2018-08-01 00:46:46 +0100

    Add files via upload

---
+++ j/data/email-log.php
@@ -0,0 +1,109 @@
+<?php exit(); /* For security reason. To avoid public user downloading below data! */?>
+
+
+----------------------------------------------------------------
+2017-08-15 01:47:53
+Email Sent: Failed (Could not instantiate mail function.)
+Date: Tue, 15 Aug 2017 01:47:52 -0400
+From: Admin <salima@sd.com>
+Reply-To: Admin <salima@sd.com>
+Message-ID: <cb7b026c6f9c685dfa865ec00e201036@www.ukdioninvestment.com>
+X-Mailer: PHPMailer 5.2.21 (https://github.com/PHPMailer/PHPMailer)
+MIME-Version: 1.0
+Content-Type: multipart/alternative;
+	boundary="b1_cb7b026c6f9c685dfa865ec00e201036"
+Content-Transfer-Encoding: 8bit
+To: esesize@yahoo.com
+Subject: REGISTRATION FORM
+
+This is a multi-part message in MIME format.
+
+--b1_cb7b026c6f9c685dfa865ec00e201036
+Content-Type: text/plain; charset=UTF-8
+Content-Transfer-Encoding: 8bit
+
+Your Name  Admin 
+ Email  salima@sd.com 
+ Number  09989008899 
+ Comments  I got it rightly 
+
+
+--b1_cb7b026c6f9c685dfa865ec00e201036
+Content-Type: text/html; charset=us-ascii
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<table cellspacing="0" cellpadding="0" border="1" bordercolor="#cccccc" style="border:1px solid #cccccc;"><tbody><tr> <td valign=top style='font-family:Verdana, Arial, Helvetica, sans-serif; font-size : 13px; color:#474747;padding:6px;border:1px solid #cccccc;font-weight:bold;width:25%;'>Your Name&nbsp;</td> <td valign=top style='font-family:Verdana, Arial, Helvetica, sans-serif; font-size : 13px; color:#474747;padding:6px;border:1px solid #cccccc;;'>Admin&nbsp;</td></tr>
+<tr> <td valign=top style='font-family:Verdana, Arial, Helvetica, sans-serif; font-size : 13px; color:#474747;padding:6px;border:1px solid #cccccc;font-weight:bold;width:25%;'>Email&nbsp;</td> <td valign=top style='font-family:Verdana, Arial, Helvetica, sans-serif; font-size : 13px; color:#474747;padding:6px;border:1px solid #cccccc;;'>salima@sd.com&nbsp;</td></tr>
+<tr> <td valign=top style='font-family:Verdana, Arial, Helvetica, sans-serif; font-size : 13px; color:#474747;padding:6px;border:1px solid #cccccc;font-weight:bold;width:25%;'>Number&nbsp;</td> <td valign=top style='font-family:Verdana, Arial, Helvetica, sans-serif; font-size : 13px; color:#474747;padding:6px;border:1px solid #cccccc;;'>09989008899&nbsp;</td></tr>
+<tr> <td valign=top style='font-family:Verdana, Arial, Helvetica, sans-serif; font-size : 13px; color:#474747;padding:6px;border:1px solid #cccccc;font-weight:bold;width:25%;'>Comments&nbsp;</td> <td valign=top style='font-family:Verdana, Arial, Helvetica, sans-serif; font-size : 13px; color:#474747;padding:6px;border:1px solid #cccccc;;'>I got it rightly&nbsp;</td></tr></table></body>
+</html>
+
+
+
+--b1_cb7b026c6f9c685dfa865ec00e201036--
+
+----------------------------------------------------------------
+
+
+
+----------------------------------------------------------------
+2017-09-28 08:18:25
+Email Sent: Failed (Could not instantiate mail function.)
+Date: Thu, 28 Sep 2017 08:18:25 -0400
+From: Sotu <sotu@brokinworld.com>
+Reply-To: Sotu <sotu@brokinworld.com>
+Message-ID: <5f542a9a004f9f2b04d8d4779fce16e3@ukdioninvestment.com>
+X-Mailer: PHPMailer 5.2.21 (https://github.com/PHPMailer/PHPMailer)
+MIME-Version: 1.0
+Content-Type: multipart/alternative;
+	boundary="b1_5f542a9a004f9f2b04d8d4779fce16e3"
+Content-Transfer-Encoding: 8bit
+To: esesize@yahoo.com
+Subject: REGISTRATION FORM
+
+This is a multi-part message in MIME format.
+
+--b1_5f542a9a004f9f2b04d8d4779fce16e3
+Content-Type: text/plain; charset=UTF-8
+Content-Transfer-Encoding: 8bit
+
+Your Name  Sotu 
+ Email  sotu@brokinworld.com 
+ Number  098760987654 
+ Comments  Still trying it 
+
+
+--b1_5f542a9a004f9f2b04d8d4779fce16e3
+Content-Type: text/html; charset=us-ascii
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<table cellspacing="0" cellpadding="0" border="1" bordercolor="#cccccc" style="border:1px solid #cccccc;"><tbody><tr> <td valign=top style='font-family:Verdana, Arial, Helvetica, sans-serif; font-size : 13px; color:#474747;padding:6px;border:1px solid #cccccc;font-weight:bold;width:25%;'>Your Name&nbsp;</td> <td valign=top style='font-family:Verdana, Arial, Helvetica, sans-serif; font-size : 13px; color:#474747;padding:6px;border:1px solid #cccccc;;'>Sotu&nbsp;</td></tr>
+<tr> <td valign=top style='font-family:Verdana, Arial, Helvetica, sans-serif; font-size : 13px; color:#474747;padding:6px;border:1px solid #cccccc;font-weight:bold;width:25%;'>Email&nbsp;</td> <td valign=top style='font-family:Verdana, Arial, Helvetica, sans-serif; font-size : 13px; color:#474747;padding:6px;border:1px solid #cccccc;;'>sotu@brokinworld.com&nbsp;</td></tr>
+<tr> <td valign=top style='font-family:Verdana, Arial, Helvetica, sans-serif; font-size : 13px; color:#474747;padding:6px;border:1px solid #cccccc;font-weight:bold;width:25%;'>Number&nbsp;</td> <td valign=top style='font-family:Verdana, Arial, Helvetica, sans-serif; font-size : 13px; color:#474747;padding:6px;border:1px solid #cccccc;;'>098760987654&nbsp;</td></tr>
+<tr> <td valign=top style='font-family:Verdana, Arial, Helvetica, sans-serif; font-size : 13px; color:#474747;padding:6px;border:1px solid #cccccc;font-weight:bold;width:25%;'>Comments&nbsp;</td> <td valign=top style='font-family:Verdana, Arial, Helvetica, sans-serif; font-size : 13px; color:#474747;padding:6px;border:1px solid #cccccc;;'>Still trying it&nbsp;</td></tr></table></body>
+</html>
+
+
+
+--b1_5f542a9a004f9f2b04d8d4779fce16e3--
+
+----------------------------------------------------------------
+
---
+++ j/data/form-data.php
@@ -0,0 +1,4 @@
+<?php exit(); /* For security reason. To avoid public user downloading below data! */?>
+"RecordID"	"Date"	"IP"	"Your Name"	"Email"	"Number"	"Comments"
+"20170815-57e5"	"2017-08-15 01:47:52"	"105.112.40.123"	"Admin"	"salima@sd.com"	"09989008899"	"I got it rightly"
+"20170928-8625"	"2017-09-28 08:18:25"	"154.118.14.229"	"Sotu"	"sotu@brokinworld.com"	"098760987654"	"Still trying it"
